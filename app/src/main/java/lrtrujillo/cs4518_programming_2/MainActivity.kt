7d629be69e81c4b5cc26fb0868f195e41b2d8508
Luke Trujillo <lrtrujillo@wpi.edu>
2020-09-18 08:17:17 -0400

    finished programming 3 part 2

--- app/src/main/java/lrtrujillo/cs4518_programming_2/MainActivity.kt
+++ app/src/main/java/lrtrujillo/cs4518_programming_2/MainActivity.kt
@@ -15,115 +15,20 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 
-private const val SCORE_A = "score_a"
-private const val SCORE_B = "score_b"
-
-private const val CUTE_DOG_REQUEST_CODE = 1;
-
 class MainActivity : AppCompatActivity() {
-
-    private val TAG = "MainActivity";
-
-    private lateinit var teamAThreePointButton: Button
-    private lateinit var teamATwoPointButton: Button
-    private lateinit var teamAFreethrowButton: Button
-
-    private lateinit var teamBThreePointButton: Button
-    private lateinit var teamBTwoPointButton: Button
-    private lateinit var teamBFreethrowButton: Button
-
-    private lateinit var saveButton: Button;
-    private lateinit var displayButton: Button;
-
-    private lateinit var resetButton: Button
-
-    private lateinit var teamAScore: TextView
-    private lateinit var teamBScore: TextView
-
-    private val game: BasketballGameViewModel by lazy {
-        ViewModelProviders.of(this).get(BasketballGameViewModel::class.java)
-    }
+   private val TAG = "MainActivity";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        Log.d("MainActivity", "onCreate(savedInstanceState: Bundle?) called");
-
         setContentView(R.layout.activity_main)
 
-        teamAThreePointButton = findViewById(R.id.teamAThreePointBtn)
-        teamATwoPointButton = findViewById(R.id.teamATwoPointBtn)
-        teamAFreethrowButton = findViewById(R.id.teamAFreeThrowBtn)
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_scoreboard);
 
-        teamBThreePointButton = findViewById(R.id.teamBThreePointBtn)
-        teamBTwoPointButton = findViewById(R.id.teamBTwoPointBtn)
-        teamBFreethrowButton = findViewById(R.id.teamBFreeThrowBtn)
-
-        displayButton = findViewById(R.id.displayButton);
-        saveButton = findViewById(R.id.saveButton);
-
-        resetButton = findViewById(R.id.resetButton)
-
-        val currentScoreA = savedInstanceState?.getInt(SCORE_A, 0) ?: 0
-        val currentScoreB = savedInstanceState?.getInt(SCORE_B, 0) ?: 0
-
-        game.teamAScore = currentScoreA
-        game.teamBScore = currentScoreB
-
-
-        teamAScore = findViewById(R.id.teamAScore)
-        teamBScore = findViewById(R.id.teamBScore)
-        updateScores();
-
-
-        saveButton.setOnClickListener {view: View ->
-
-            Log.d(TAG, "saveButton.setOnClickListenerCalled()");
-
-            val intent = Intent(this, CuteDogActivity::class.java);
-            intent.putExtra("TEAM_A_NAME", teamALabel.text);
-            intent.putExtra("TEAM_B_NAME", teamBLabel.text);
-            intent.putExtra("TEAM_A_SCORE", game.getScoreTeamA());
-            intent.putExtra("Team_B_SCORE", game.getScoreTeamB());
-
-            startActivityForResult(intent, CUTE_DOG_REQUEST_CODE);
+        if(currentFragment == null) {
+            val fragment = ScoreboardFragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_scoreboard, fragment).commit()
         }
-
-
-
-        teamAThreePointButton.setOnClickListener { view: View ->
-            game.addScoreTeamA(3);
-            updateScores()
-        }
-        teamATwoPointButton.setOnClickListener { view: View ->
-            game.addScoreTeamA(2);
-            updateScores()
-        }
-        teamAFreethrowButton.setOnClickListener { view: View ->
-            game.addScoreTeamA(1);
-            updateScores()
-        }
-        teamBThreePointButton.setOnClickListener { view: View ->
-            game.addScoreTeamB(3);
-            updateScores()
-        }
-        teamBTwoPointButton.setOnClickListener { view: View ->
-            game.addScoreTeamB(2);
-            updateScores()
-        }
-        teamBFreethrowButton.setOnClickListener { view: View ->
-            game.addScoreTeamB(1);
-            updateScores()
-        }
-        resetButton.setOnClickListener { view: View ->
-            game.reset()
-            updateScores()
-        }
-
-        val provider: ViewModelProvider = ViewModelProviders.of(this)
-        val basketballGameViewModel = provider.get(BasketballGameViewModel::class.java)
-
-        }
+    }
 
     override fun onPause() {
         super.onPause()
@@ -138,18 +43,6 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         Log.d("MainActivity", "onPause() called")
-    }
-    override fun onSaveInstanceState(savedInstanceState: Bundle) {
-        super.onSaveInstanceState(savedInstanceState);
-        Log.d("MainActivity", "onSaveInstanceState(savedInstanceState: Bundle) called");
-
-        savedInstanceState.putInt(SCORE_A, game.getScoreTeamA())
-        savedInstanceState.putInt(SCORE_B, game.getScoreTeamB())
-    }
-
-    private fun updateScores() {
-        teamAScore.setText(game.getScoreTeamA().toString())
-        teamBScore.setText(game.getScoreTeamB().toString())
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
--- app/src/main/java/lrtrujillo/cs4518_programming_2/ScoreboardFragment.kt
+++ app/src/main/java/lrtrujillo/cs4518_programming_2/ScoreboardFragment.kt
@@ -1,15 +1,30 @@
 package lrtrujillo.cs4518_programming_2
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+
+private const val SCORE_A = "score_a"
+private const val SCORE_B = "score_b"
+
+private const val CUTE_DOG_REQUEST_CODE = 1;
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +32,30 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ScoreboardFragment : Fragment() {
+
+    private val TAG = "ScoreboardFragment";
+
+    private lateinit var teamAThreePointButton: Button
+    private lateinit var teamATwoPointButton: Button
+    private lateinit var teamAFreethrowButton: Button
+
+    private lateinit var teamBThreePointButton: Button
+    private lateinit var teamBTwoPointButton: Button
+    private lateinit var teamBFreethrowButton: Button
+
+    private lateinit var saveButton: Button;
+    private lateinit var displayButton: Button;
+
+    private lateinit var resetButton: Button
+
+    private lateinit var teamAScore: TextView
+    private lateinit var teamBScore: TextView
+
+    private val game: BasketballGameViewModel by lazy {
+        ViewModelProviders.of(this).get(BasketballGameViewModel::class.java)
+    }
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -27,14 +66,104 @@ class ScoreboardFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        Log.d(TAG, "onCreate(savedInstanceState: Bundle?) called");
+
     }
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d("MainActivity", "onSaveInstanceState(savedInstanceState: Bundle) called");
+
+        savedInstanceState.putInt(SCORE_A, game.getScoreTeamA())
+        savedInstanceState.putInt(SCORE_B, game.getScoreTeamB())
+    }
+
+    private fun updateScores() {
+        teamAScore.setText(game.getScoreTeamA().toString())
+        teamBScore.setText(game.getScoreTeamB().toString())
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d(TAG, "onCreateView() called.");
+        val view = inflater.inflate(R.layout.fragment_scoreboard, container, false);
+
+        teamAThreePointButton = view.findViewById(R.id.teamAThreePointBtn) as Button
+        teamATwoPointButton = view.findViewById(R.id.teamATwoPointBtn) as Button
+        teamAFreethrowButton = view.findViewById(R.id.teamAFreeThrowBtn) as Button
+
+        teamBThreePointButton = view.findViewById(R.id.teamBThreePointBtn) as Button
+        teamBTwoPointButton = view.findViewById(R.id.teamBTwoPointBtn) as Button
+        teamBFreethrowButton = view.findViewById(R.id.teamBFreeThrowBtn) as Button
+
+        displayButton = view.findViewById(R.id.displayButton) as Button
+        saveButton = view.findViewById(R.id.saveButton) as Button
+
+        resetButton = view.findViewById(R.id.resetButton) as Button
+
+        val currentScoreA = savedInstanceState?.getInt(SCORE_A, 0) ?: 0
+        val currentScoreB = savedInstanceState?.getInt(SCORE_B, 0) ?: 0
+
+        game.teamAScore = currentScoreA
+        game.teamBScore = currentScoreB
+
+        teamAScore = view.findViewById(R.id.teamAScore)
+        teamBScore = view.findViewById(R.id.teamBScore)
+        updateScores();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scoreboard, container, false)
+        return view;
+    }
+
+    override fun onStart() {
+        super.onStart();
+
+        saveButton.setOnClickListener {view: View ->
+
+            Log.d(TAG, "saveButton.setOnClickListenerCalled()");
+
+            val intent = Intent(activity, CuteDogActivity::class.java);
+            intent.putExtra("TEAM_A_NAME", teamALabel.text);
+            intent.putExtra("TEAM_B_NAME", teamBLabel.text);
+            intent.putExtra("TEAM_A_SCORE", game.getScoreTeamA());
+            intent.putExtra("TEAM_B_SCORE", game.getScoreTeamB());
+
+            startActivityForResult(intent, CUTE_DOG_REQUEST_CODE);
+        }
+
+
+
+        teamAThreePointButton.setOnClickListener { view: View ->
+            game.addScoreTeamA(3);
+            updateScores()
+        }
+        teamATwoPointButton.setOnClickListener { view: View ->
+            game.addScoreTeamA(2);
+            updateScores()
+        }
+        teamAFreethrowButton.setOnClickListener { view: View ->
+            game.addScoreTeamA(1);
+            updateScores()
+        }
+        teamBThreePointButton.setOnClickListener { view: View ->
+            game.addScoreTeamB(3);
+            updateScores()
+        }
+        teamBTwoPointButton.setOnClickListener { view: View ->
+            game.addScoreTeamB(2);
+            updateScores()
+        }
+        teamBFreethrowButton.setOnClickListener { view: View ->
+            game.addScoreTeamB(1);
+            updateScores()
+        }
+        resetButton.setOnClickListener { view: View ->
+            game.reset()
+            updateScores()
+        }
     }
 
     companion object {
